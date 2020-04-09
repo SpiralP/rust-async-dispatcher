488b53c49d65737406b1eee925a379152e087d30
SpiralP <spiral.palmer@gmail.com>
2020-04-09 15:25:19 -0700

    convert to use futures

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,192 @@
-use crossbeam_channel::{unbounded, Receiver, Sender};
-use futures::channel::oneshot::channel as oneshot_channel;
+use futures::{channel::oneshot, executor::*, future::BoxFuture, prelude::*, task::SpawnExt};
+use std::{
+    fmt::Debug,
+    sync::mpsc::{channel, Receiver, Sender},
+};
 
-type Task = Box<dyn FnOnce() + Send>;
+type Task = BoxFuture<'static, ()>;
 
 /// Used to call functions from another thread, getting their return values via `await`.
 pub struct Dispatcher {
-  task_sender: Sender<Task>,
-  task_receiver: Receiver<Task>,
+    task_sender: Sender<Task>,
+    task_receiver: Receiver<Task>,
+    future_pool: LocalPool,
+    future_spawner: LocalSpawner,
 }
 
 impl Default for Dispatcher {
-  fn default() -> Self {
-    Self::new()
-  }
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Dispatcher {
-  pub fn new() -> Self {
-    let (task_sender, task_receiver) = unbounded();
+    pub fn new() -> Self {
+        let (task_sender, task_receiver) = channel();
 
-    Self {
-      task_sender,
-      task_receiver,
-    }
-  }
+        let future_pool = LocalPool::new();
+        let future_spawner = future_pool.spawner();
 
-  /// Run all functions currently in the queue.
-  pub fn process_tasks(&mut self) {
-    for task in self.task_receiver.try_iter() {
-      task();
+        Self {
+            task_sender,
+            task_receiver,
+            future_pool,
+            future_spawner,
+        }
     }
-  }
 
-  /// Get a [`DispatcherHandle`] to control this Dispatcher from another thread.
-  pub fn get_handle(&self) -> DispatcherHandle {
-    DispatcherHandle {
-      task_sender: self.task_sender.clone(),
+    fn spawn_queued(&mut self) {
+        for task in self.task_receiver.try_iter() {
+            self.future_spawner.spawn(task).unwrap();
+        }
     }
-  }
+
+    /// Run all tasks in the pool to completion.
+    pub fn run(&mut self) {
+        self.spawn_queued();
+
+        self.future_pool.run();
+    }
+
+    /// Runs all tasks in the pool and returns if no more progress can be made on any task.
+    pub fn run_until_stalled(&mut self) {
+        self.spawn_queued();
+
+        self.future_pool.run_until_stalled();
+    }
+
+    /// Runs all tasks and returns after completing one future or until no more progress can be made. Returns true if one future was completed, false otherwise.
+    pub fn try_run_one(&mut self) {
+        self.spawn_queued();
+
+        self.future_pool.try_run_one();
+    }
+
+    /// Get a [`DispatcherHandle`] to control this Dispatcher from another thread.
+    pub fn get_handle(&self) -> DispatcherHandle {
+        DispatcherHandle {
+            task_sender: self.task_sender.clone(),
+        }
+    }
 }
 
 /// Created by calling [`Dispatcher::get_handle`].
 pub struct DispatcherHandle {
-  task_sender: Sender<Task>,
+    task_sender: Sender<Task>,
 }
 
 impl DispatcherHandle {
-  fn spawn<F: FnOnce() + Send + 'static>(&mut self, f: F) {
-    self.task_sender.send(Box::new(f)).unwrap();
-  }
+    /// Enqueue a function to be ran when [`Dispatcher::process_tasks`] is called.
+    ///
+    /// This function
+    pub fn spawn<F>(&mut self, future: F)
+    where
+        F: Future<Output = ()> + 'static + Send,
+    {
+        self.task_sender.send(future.boxed()).unwrap();
+    }
 
-  /// Enqueue a function to be ran when [`Dispatcher::process_tasks`] is called.
-  ///
-  /// This returns a Future that resolves to the function's return value.
-  pub async fn dispatch<T, F>(&mut self, f: F) -> T
-  where
-    T: Send + 'static,
-    F: (FnOnce() -> T) + Send + 'static,
-  {
-    let (oneshot_sender, oneshot_receiver) = oneshot_channel();
+    /// Enqueue a function to be ran when [`Dispatcher::process_tasks`] is called.
+    ///
+    /// This returns a Future that resolves to the function's return value.
+    pub async fn dispatch<F, O>(&mut self, future: F) -> O
+    where
+        F: Future<Output = O> + 'static + Send,
+        O: 'static + Send + Debug,
+    {
+        let (return_sender, return_receiver) = oneshot::channel();
 
-    self.spawn(move || {
-      let _ignore_error = oneshot_sender.send(f());
-    });
+        self.spawn(
+            async {
+                let ret = future.await;
+                return_sender.send(ret).unwrap();
+            }
+            .boxed(),
+        );
 
-    oneshot_receiver.await.unwrap()
-  }
+        return_receiver.await.unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use futures::executor::block_on;
-  use std::{thread, time::Duration};
 
-  #[test]
-  fn it_works() {
-    let mut main_thread_dispatcher = Dispatcher::new();
-    let mut handle = main_thread_dispatcher.get_handle();
+    use super::*;
+    use futures::executor::block_on;
+    use std::{thread, time::Duration};
 
-    let t = thread::spawn(move || {
-      block_on(async move {
-        handle
-          .dispatch(move || {
-            println!("MAIN THREAD HI");
+    #[test]
+    fn it_works() {
+        println!(
+            "dispatcher running on thread {:?}",
+            thread::current().name()
+        );
+        let mut main_thread_dispatcher = Dispatcher::new();
 
-            99
-          })
-          .await
-      })
-    });
+        let mut handle = main_thread_dispatcher.get_handle();
+        let t = thread::spawn(move || {
+            block_on(async move {
+                println!(
+                    "new thread running on thread {:?}",
+                    thread::current().name()
+                );
 
-    thread::sleep(Duration::from_millis(1000));
-    main_thread_dispatcher.process_tasks();
+                println!("dispatching");
+                handle
+                    .dispatch(async {
+                        println!(
+                            "dispatched task running on thread {:?}",
+                            thread::current().name()
+                        );
 
-    assert!(t.join().unwrap() == 99);
-  }
+                        99
+                    })
+                    .await
+            })
+        });
+
+        // wait for thread to dispatch
+        thread::sleep(Duration::from_secs(1));
+
+        println!("running dispatcher");
+        main_thread_dispatcher.run();
+        println!("dispatcher done running");
+
+        assert!(t.join().unwrap() == 99);
+    }
+
+    #[test]
+    fn test_tokio() {
+        // you can't use tokio timers in these threads though!
+
+        let mut main_thread_dispatcher = Dispatcher::new();
+        let mut handle = main_thread_dispatcher.get_handle();
+
+        let tokio_thread = thread::spawn(move || {
+            let mut rt = tokio::runtime::Runtime::new().unwrap();
+
+            rt.block_on(async move {
+                println!("dispatching");
+                handle
+                    .dispatch(async {
+                        println!(
+                            "dispatched task running on thread {:?}",
+                            thread::current().name()
+                        );
+
+                        99u8
+                    })
+                    .await
+            })
+        });
+
+        // wait for thread to dispatch
+        thread::sleep(Duration::from_secs(1));
+
+        println!("running dispatcher");
+        main_thread_dispatcher.run();
+        println!("dispatcher done running");
+
+        assert!(tokio_thread.join().unwrap() == 99);
+    }
 }
